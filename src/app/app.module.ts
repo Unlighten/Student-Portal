ded2f3fd00b2d925d6cf62525d849f54fdb53d33
Aaron Moore <x2kicker2x@gmail.com>
2017-10-09 15:08:09 -0400

    Working on login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HomeStudentComponent } from './admin/home/home-student/home-student.com
 import { HomeItemComponent } from './admin/home/home-list/home-item/home-item.component';
 import { CreateEditComponent } from './admin/create-assignment/create-edit/create-edit.component';
 import { FooterComponent } from './footer/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import { FooterComponent } from './footer/footer/footer.component';
     HomeStudentComponent,
     HomeItemComponent,
     CreateEditComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
