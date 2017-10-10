a1d68d3fd739a1f420d46b2d1cb7075e2cd48820
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-10-10 13:36:33 -0400

    Added forms

--- src/app/admin/create-assignment/create-edit/create-edit.component.ts
+++ src/app/admin/create-assignment/create-edit/create-edit.component.ts
@@ -1,30 +1,62 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Assignment } from '../../../shared/assignment.model';
 import { CreateAssignmentService } from '../create-assignment.service';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-create-edit',
   templateUrl: './create-edit.component.html',
   styleUrls: ['./create-edit.component.css']
 })
-export class CreateEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('descInput') descInputRef: ElementRef;
-  @ViewChild('dueInput') dueInputRef: ElementRef;
+export class CreateEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') createAssignmentForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Assignment;
 
   constructor(private createAssignmentService: CreateAssignmentService) { }
 
   ngOnInit() {
+    this.subscription = this.createAssignmentService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.createAssignmentService.getAssignment(index);
+        this.createAssignmentForm.setValue({
+          name: this.editedItem.name,
+          desc: this.editedItem.description,
+          due: this.editedItem.due
+        })
+      }
+    );
   }
 
-  onAddItem() {
-    const assignName = this.nameInputRef.nativeElement.value;
-    const assignDesc = this.descInputRef.nativeElement.value;
-    const assignDue = this.dueInputRef.nativeElement.value;
-    
-    const newAssignment = new Assignment(assignName, assignDesc, assignDue);
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newAssignment = new Assignment(value.name, value.desc, value.due);
+    if (this.editMode) {
+      this.createAssignmentService.updateAssignment(this.editedItemIndex, newAssignment);
+    } else {
+      this.createAssignmentService.addAssignment(newAssignment);
+    }
+    this.editMode = false;
+    form.reset();
+  }
 
-    this.createAssignmentService.addAssignment(newAssignment);
+  onClear() {
+    this.createAssignmentForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.createAssignmentService.deleteAssignment(this.editedItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
