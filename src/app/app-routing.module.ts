891774d3ab4c542ea75142349a8eea9babad6bbd
Nathan Bogan <bogannathan@gmail.com>
2017-10-10 12:06:44 -0400

    fixed login routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
+import { HomeComponent } from "./admin/home/home.component"
 import { AuthGuard } from "./auth/auth-guard.service";
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent }
 ];
 
