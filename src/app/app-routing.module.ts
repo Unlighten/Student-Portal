dde2e02bf932313918003fa72b74199c4baa714b
Aaron Moore <x2kicker2x@gmail.com>
2017-10-10 11:55:46 -0400

    Working with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
 import { AuthGuard } from "./auth/auth-guard.service";
+import { CreateEditComponent } from './admin/create-assignment/create-edit/create-edit.component';
+import { CreateAssignmentComponent } from './admin/create-assignment/create-assignment.component';
+
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
