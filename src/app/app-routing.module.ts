3dd69be027b9b036fc023e379da20580bb19e3b1
Aaron Moore <x2kicker2x@gmail.com>
2017-10-10 11:26:11 -0400

    Login works! We got it boys!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,5 +15,5 @@ const appRoutes: Routes = [
 })
 
 export class AppRoutingModule {
-    
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import firebaseConfig from '../../firebaseConfig.js';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,17 @@ import firebaseConfig from '../../firebaseConfig.js';
 })
 export class AppComponent {
   loadedFeature = 'home';
+
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: firebaseConfig.apiKey,
+      authDomain: firebaseConfig.authDomain,
+      databaseURL: firebaseConfig.databaseURL,
+      projectId: firebaseConfig.projectId,
+      storageBucket: firebaseConfig.storageBucket,
+      messagingSenderId: firebaseConfig.messagingSenderId
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
