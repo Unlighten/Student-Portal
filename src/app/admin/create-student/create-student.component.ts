1447407a54c8ac4c4553cb91fad366ad4b8a14c4
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-10-17 13:25:56 -0400

    CRUD functionality added to create-students

---
+++ src/app/admin/create-student/create-student.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../../shared/data-storage.service';
+
+@Component({
+  selector: 'app-create-student',
+  templateUrl: './create-student.component.html',
+  styleUrls: ['./create-student.component.css']
+})
+export class CreateStudentComponent implements OnInit {
+
+  constructor(private dataStorageService: DataStorageService) { }
+
+  ngOnInit() {
+    this.onFetchData(); //Fetches data from FB onInit
+  }
+
+  onFetchData() {
+    this.dataStorageService.getData(); //Attn. data-storage.service.ts
+  }
+}
