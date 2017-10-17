0dbb81946ed22f10e11d0264a58e89e416c8e1f7
Nathan Bogan <bogannathan@gmail.com>
2017-10-17 11:15:39 -0400

    initial commit

---
+++ src/app/admin/add-cohort/add-cohort.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-cohort',
+  templateUrl: './add-cohort.component.html',
+  styleUrls: ['./add-cohort.component.css']
+})
+export class AddCohortComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
