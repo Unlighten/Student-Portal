2f7bdf3c8d04be10fd956c866ee87cc9894a044a
Aaron Moore <x2kicker2x@gmail.com>
2017-10-19 11:21:31 -0400

    Uploading assignments is closer to working

--- src/app/home/home-assignment/assignment-detail/assignment-detail.component.ts
+++ src/app/home/home-assignment/assignment-detail/assignment-detail.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Assignment } from '../../../shared/assignment.model';
+import { CAssignment } from './add-completed-assignment.model';
 import { NgForm } from '@angular/forms';
 import { DataStorageService } from '../../../shared/data-storage.service';
 import { Subscription } from 'rxjs/Subscription';
 import { CreateAssignmentService } from '../../../admin/create-assignment/create-assignment.service';
+import { Response } from '@angular/http';
+import { AddCAssignmentService } from './add-completed-assignment.service'
 
 @Component({
   selector: 'app-assignment-detail',
@@ -11,7 +14,9 @@ import { CreateAssignmentService } from '../../../admin/create-assignment/create
   styleUrls: ['./assignment-detail.component.css']
 })
 export class AssignmentDetailComponent implements OnInit {
+  AddCAssignmentService: any;  //aaron/wonky
   assignments: Assignment[];
+  caAssignments: CAssignment[]; //aaron/wonky
   private subscription: Subscription;
   assignment: Assignment = { //Empty object to fill with modal click
     name: '',
@@ -20,10 +25,13 @@ export class AssignmentDetailComponent implements OnInit {
     cohort: ''
   };
   
-  constructor(private createAssignmentService: CreateAssignmentService, private dataStorageService: DataStorageService,) { }
+  constructor(private createAssignmentService: CreateAssignmentService, private dataStorageService: DataStorageService, private addCAssignmentService: AddCAssignmentService) { }
 
   ngOnInit() { //Creates and infills assignments onInit
     this.assignments = this.createAssignmentService.getAssignments();
+    // console.log('^ this is printing stuff ^', this.assignments);
+    this.caAssignments = this.addCAssignmentService.getCompletedAssignment(); //aaron/wonky
+    // console.log('^ this is printing essentially nothing ^', this.caAssignments);
     this.subscription = this.createAssignmentService.assignmentsChanged.subscribe(
       (assignments: Assignment[]) => {
         this.assignments = assignments;
@@ -34,20 +42,19 @@ export class AssignmentDetailComponent implements OnInit {
     this.createAssignmentService.oneAssignment.subscribe(data => this.assignment = data); //Modal component => Attn. createAssignmentService
   }
 
-//this is where youi will need to edit some stuff to be able to submit the assignments
 
   onSubmit(form: NgForm) {
     const completedAssignment = form.value.completedAssignment;
-    // this.addCohortService.addCohort(newCohort)
+    this.addCAssignmentService.addCompletedAssignment(completedAssignment)
     this.onSaveData()
-    // form.reset();
+    form.reset();
   }
 
   onSaveData() {
     this.dataStorageService.storeCompletedAssignmentData().subscribe(
-      // (response: Response) => {
-      //   console.log(response);
-      // }
+      (response: Response) => {
+        console.log(response);
+      }
     );
   }
 }
