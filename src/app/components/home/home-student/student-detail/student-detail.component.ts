182250ab8632875bd99a9afff650132c745bae18
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-10-26 09:55:52 -0400

    Student detail started

--- src/app/components/home/home-student/student-detail/student-detail.component.ts
+++ src/app/components/home/home-student/student-detail/student-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription'
 import { Student } from '../../../../models/student.model';
 import { StudentService } from '../../../../services/student.service';
+import { AuthService } from '../../../../auth/auth.service';
+import { CohortService } from '../../../../services/cohort.service';
 
 @Component({
   selector: 'app-student-detail',
@@ -9,19 +11,31 @@ import { StudentService } from '../../../../services/student.service';
   styleUrls: ['./student-detail.component.css']
 })
 export class StudentDetailComponent implements OnInit {
+  studentAssignments: any;
   student: Student = {
     fname: '',
     lname: '',
     email: '',
-    cohort: ''
+    cohort: '',
+    // studentAssignments: {}
   };
+  uid
+  studentKey
+  cohorts
+  cohort
+  cohortKey
+  assignmentKey
+  // studentAssignments
   students: Student[];
+
   private subscription: Subscription;
+  private cohortSubscription: Subscription;
 
-  constructor(private studentService: StudentService) { }
+  constructor(private studentService: StudentService, private authService: AuthService, private cohortService: CohortService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.students = this.studentService.getStudents();
+    // this.uid = this.authService.getUID();
     this.subscription = this.studentService.studentsChanged.subscribe(
       (students: Student[]) => {
         this.students = students;
